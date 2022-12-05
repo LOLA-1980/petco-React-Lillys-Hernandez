032e28ce550dcf68cb0d7f3c44ebe2b0ae12dfeb
Lillys Hernandez <lillys.hernandez.ramos@gmail.com>
2022-12-05 13:03:12 -0600

    Segunda Preentrega proyecto React

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from "./components/Error404/Error404";
 import Footer from "./components/Footer/Footer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Banner from "./components/Banner/Banner";
+//import Banner from "./components/Banner/Banner";
 import Inicio from "./components/Inicio/Inicio";
 import './App.css';
 
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <Navbar />
-      <ItemListContainer greeting={"¡Bienvenidos a P E T C O !!!"} />
-      <div className="container">
+      <Routes>
+        <Route path={"/"} element={<ItemListContainer />} />
+        <Route path={"/category/:id"} element={<ItemListContainer />} />
+        <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+        <Route path={"*"} element={<Error404 />} />
+      </Routes>
+      {/*<div className="container">
         <Banner />
-      </div>
-      <Inicio></Inicio>
+      </div>*/}
+      {/*<Inicio></Inicio>*/}
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
